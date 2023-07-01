f49fadea1090ea182aa4691d21bf9c42cfce301a
Aleksandar Zivanovic <acas_mail@yahoo.com>
2023-07-01 09:23:18 +0200

    14. Twig Templates (added Twig package)

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,24 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Component\DependencyInjection\Parameter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = ['Hi', 'Hello', 'Bye!'];
 
     #[Route('/{limit}', name: 'app_index', requirements: ['limit' => '\d+'])]
     public function index(int $limit = 1): Response
     {
-        return new Response(implode(', ', array_slice($this->messages, 0, $limit)));
+        return $this->render('hello/index.html.twig', [
+            'message' => implode(', ', array_slice($this->messages, 0, $limit)),
+        ]);
     }
 
     #[Route('/messages/{id}', name: 'app_show_one', requirements: ['id' => '\d+'])]
     // #[Route('/messages/{id<\d+>}', name: 'app_show_one')] // - isto kao kod iznad
     public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        // return new Response($this->messages[$id]);
+        return $this->render('hello\showOne.html.twig', [
+            'message' => $this->messages[$id],
+        ]);
     }
 }
