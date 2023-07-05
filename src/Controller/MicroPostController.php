898c2f5a4bce8eb328a0497eca8b8d510705aff4
Aleksandar Zivanovic <acas_mail@yahoo.com>
2023-07-05 16:13:00 +0200

    31. Project - Getting Posts From Database

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,31 +12,17 @@ class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
-    {
-
-        // $microPost = new MicroPost();
-        // $microPost->setTitle('Created from the controller!');
-        // $microPost->setText('Hi!');
-        // $microPost->setCreated(new DateTime());
-        // $posts->save($microPost, true);
-
-        // $microPost = $posts->find(2);
-        // $microPost->setTitle('Welcome to Disneyland!');
-        // $posts->save($microPost, true);
-
-        // $microPost = $posts->find(4);
-        // $posts->remove($microPost, true);
-
-        // dd($posts->findOneBy(['title' => 'Welcome to US!']));
-        
+    {   
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
         ]);
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show', requirements: ['id' => '\d+'])]
-    public function showOne(MicroPost $post)
+    public function showOne(MicroPost $post): Response
     {
-        dd($post);
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
+        ]);
     }
 }
